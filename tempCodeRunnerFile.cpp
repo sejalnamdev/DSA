d5a042aa97f6653f846b1f75646873ab84e0f72b
sejalnamdev <namdevsejal31@gmail.com>
2023-06-06 19:04:07 +0530

    initial commit

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+   int num1=8;
+   int num2=4;
+
+   
+   cout<<num1+num2<<endl; //12
+   cout<<num1-num2<<endl; //4
+   cout<<num1*num2<<endl; //32
+   cout<<num1/<<endl; //false
+   cout<<(num1!=num2)<<endl; //true
+   cout<<(num1<num2)<<endl; //false
+   cout<<(num1>num2)<<endl; //true
+   cout<<(num1<=num2)<<endl; //false
+   cout<<(num1>=num2)<<endl; //true
+
+
+    return 0;
+   
+
+}
+
+
+
+
+
