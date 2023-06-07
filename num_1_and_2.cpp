20b95c340c8ca1470af80860358b08c4b0ddfb51
sejalnamdev <namdevsejal31@gmail.com>
2023-06-07 12:26:56 +0530

    initial commit

--- num_1_and_2.cpp
+++ num_1_and_2.cpp
@@ -1,32 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 
-   int num1=8;
-   int num2=4;
+   int num1 = 8;
+   int num2 = 4;
 
-   
-   cout<<num1+num2<<endl; //12
-   cout<<num1-num2<<endl; //4
-   cout<<num1*num2<<endl; //32
-   cout<<num1/num2<<endl; //2
-   cout<<num1%num2<<endl; //0
+   cout << num1 + num2 << endl; // 12
+   cout << num1 - num2 << endl; // 4
+   cout << num1 * num2 << endl; // 32
+   cout << num1 / num2 << endl; // 2
+   cout << num1 % num2 << endl; // 0
 
-   cout<<(num1==num2)<<endl; //false
-   cout<<(num1!=num2)<<endl; //true
-   cout<<(num1<num2)<<endl; //false
-   cout<<(num1>num2)<<endl; //true
-   cout<<(num1<=num2)<<endl; //false
-   cout<<(num1>=num2)<<endl; //true
+   cout << (num1 == num2) << endl; // false
+   cout << (num1 != num2) << endl; // true
+   cout << (num1 < num2) << endl;  // false
+   cout << (num1 > num2) << endl;  // true
+   cout << (num1 <= num2) << endl; // false
+   cout << (num1 >= num2) << endl; // true
 
-
-    return 0;
-   
-
+   return 0;
 }
-
-
-
-
-
