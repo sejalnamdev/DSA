f79d5d4daf664102f5cc85df8f4dcfc0a6c2b6ae
sejalnamdev <namdevsejal31@gmail.com>
2023-06-19 16:46:27 +0530

    initial commit

---
+++ while_if.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    while(true){
+
+        int n;
+        cin>>n;
+
+        if(n==-1){
+            cout<<"stop";
+        }else{
+            cout<<n;
+        }
+    }
+}
