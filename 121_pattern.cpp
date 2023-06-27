c18b86467058d4ffd093332c0776eab4fd278a13
sejalnamdev <namdevsejal31@gmail.com>
2023-06-27 19:04:06 +0530

    initial summary

--- 121_pattern.cpp
+++ 121_pattern.cpp
@@ -4,19 +4,16 @@ using namespace std;
 int main(){
     int n,m;
     cin>>n>>m;
-    for(int i=1;i<=n;i++){
-        for(int j=1; j<=m;j++){
-            if(j==1||j==m ||i==1||i==n){
-                cout<<j;
-            }
-            else {
-                cout<<" ";
-            }
-        
 
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if((i+j)%2==0){
+                cout<<"1";
+            }else{
+                cout<<"2";
+            }
         }
         cout<<endl;
-
     }
     return 0;
 }
