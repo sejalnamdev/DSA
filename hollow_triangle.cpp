c397616ad4d25efb3f7100c75a594159bdb37da9
sejalnamdev <namdevsejal31@gmail.com>
2023-06-29 22:40:42 +0530

    initial commit

---
+++ hollow_triangle.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,m;
+    cin>>n>>m;
+    for(i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if()
+            
+            
+
+        }
+        cout<<endl;
+    }
+    return 0;
+}
