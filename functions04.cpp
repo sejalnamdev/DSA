03d2ca95bf97e29b85cea3272151b9ef26d4e76e
sejalnamdev <namdevsejal31@gmail.com>
2023-07-11 22:58:47 +0530

    initial commit

---
+++ functions04.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+
+void decrease(int n1,int n2){
+    n1--;
+    n2=n2-2;
+    cout<<(n1)<<":"<<(n2)<<endl;
+    
+    //n1 and n2 are formal parameters
+}
+int main(){
+    int p=26;
+    int q=13;
+    decrease(p,q);
+    cout<<(p)<<":"<<(q)<<endl;
+    //p and q are actual parameters
+}
