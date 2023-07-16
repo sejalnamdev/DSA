21c51446a30d59e9f9c85853d9cdd844ac2a1bbc
sejal namdev <135623345+sejalnamdev@users.noreply.github.com>
2023-07-16 22:44:08 +0530

    Update Lazy_girl_sejal.cpp

--- Lazy_girl_sejal.cpp
+++ Lazy_girl_sejal.cpp
@@ -1,7 +1,9 @@
-TO MY BEST FRIEND 
-SEJAL,
-
-KL SE REAL CODE JANA CHAHIYE ◉_◉
-
-Your friend
-Sheel ◔ ⌣ ◔
+{  
+  TO MY BEST FRIEND 
+  SEJAL,
+  
+  KL SE REAL CODE JANA CHAHIYE ◉_◉
+  
+  Your friend
+  Sheel ◔ ⌣ ◔
+}
