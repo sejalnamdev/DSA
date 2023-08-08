62c89a634ba4ce0aa861ae92e3458d0749a154f7
sejalnamdev <namdevsejal31@gmail.com>
2023-08-08 15:47:59 +0530

    initial commit

---
+++ lastoccurence.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+
+    vector<int> v(6);
+    for(int i=0;i<6;i++){
+        cin<<v[i];
+    }
+
+    cout<<"Enter x:";
+    int x;
+    cin>>x;
+
+    //from start
+
+    int occurence=-1;
+    //for(int i=0;i<v.size();i++){
+    //    if(v[i]==x){
+    //        occurence=i;
+    //    }
+    //}
+    //cout<<occurence<<endl;
+
+    //from end
+
+    for(int i=v.size()-1;i>=0;i--){
+        if(v[i]==x){
+            occurence=i;
+            break;
+        }
+    }
+    cout<<occurence<<endl;
+
+
+   return 0;
+}
