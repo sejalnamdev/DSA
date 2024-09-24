7cef3e819129dd5c364d46fa65053276de50ec20
sejalnamdev <namdevsejal31@gmail.com>
2024-09-24 07:36:31 +0530

    initialcommit

--- DSA_SERIES-C++/bubblesortarray.cpp
+++ DSA_SERIES-C++/bubblesortarray.cpp
@@ -14,12 +14,27 @@ int main(){
     }
 
     for(int i=n-2;i>=0;i--){
+
+        bool swapped=0;
+
         for(int j=0;j<=i;j++){
             if(arr[j]>arr[j+1]){
+
+                swapped=1;
+
                 swap(arr[j],arr[j+1]);
             }
         }
+
+        if (swapped==0){
+            break;
+        }
+        
+
     }
+    
+
+
     for(int i=0;i<n;i++){
         
         cout<<arr[i]<<" ";
