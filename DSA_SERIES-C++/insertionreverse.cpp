405c5393d99ae3ad522ed73ca98eae1d845a136f
sejalnamdev <namdevsejal31@gmail.com>
2024-10-01 10:21:22 +0530

    initial commit

--- DSA_SERIES-C++/insertionreverse.cpp
+++ DSA_SERIES-C++/insertionreverse.cpp
@@ -2,6 +2,25 @@
 using namespace std;
 
 int main(){
-    
+    int arr[1000];
+    int n;
+    cout<<"size of array: ";
+    cin>>n;
+    cout<<"elements of array: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]<arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+            
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i];
+    }
     return 0;
 }
